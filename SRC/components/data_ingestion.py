672cee95fb6a74bf9d37c4803c80d098f8e9cde5
Aman007rj <aman007rj@gmal.com>
2023-07-30 00:02:52 +0530

    model trained

--- SRC/components/data_ingestion.py
+++ SRC/components/data_ingestion.py
@@ -7,6 +7,8 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from SRC.components.data_transformation import DataTransformation
 from SRC.components.data_transformation import DataTransformationConfig
+from SRC.components.model_trainer import ModelTrainer
+from SRC.components.model_trainer import ModelTrainerConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -46,6 +48,11 @@ class DataIngestion:
 if __name__ == "__main__":
     obj = DataIngestion()
     train_data, test_data = obj.initiate_data_ingestion()
+
     data_transfromation = DataTransformation()
-    data_transfromation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr = data_transfromation.initiate_data_transformation(train_data, test_data)
+
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
+
 
