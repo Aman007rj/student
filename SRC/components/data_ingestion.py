d3baf6382374f3ea74b3c597410bb083249db336
Aman007rj <aman007rj@gmal.com>
2023-07-29 20:09:08 +0530

    data transformation done

--- SRC/components/data_ingestion.py
+++ SRC/components/data_ingestion.py
@@ -5,6 +5,8 @@ from SRC.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from SRC.components.data_transformation import DataTransformation
+from SRC.components.data_transformation import DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -26,16 +28,15 @@ class DataIngestion:
             df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
 
             logging.info("train test split started")
-            train_test, test_set = train_test_split(df, test_size=0.2, random_state=42)
-            df.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
-            df.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
 
             logging.info('data ingestion completed')
 
             return(
                 self.ingestion_config.train_data_path,
-                self.ingestion_config.test_data_path,
-                self.ingestion_config.raw_data_path
+                self.ingestion_config.test_data_path
             )
 
         except Exception as e:
@@ -44,5 +45,7 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+    data_transfromation = DataTransformation()
+    data_transfromation.initiate_data_transformation(train_data, test_data)
 
